3db065285ca3643b8dc69f1b893268542762488d
exsemt <exsemt@gmail.com>
2014-04-06 12:55:30 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Exsemt::Application.config.secret_key_base = '2dba06fbcea1b78f7ee406f633d7b88516e6c857cbe66825d124d4bd7e2bc77de51c37eb6b3fa463b5b3d74ce3c49fccb8c3ac7ce2e4cd2e19bfbefe0f688cc1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Exsemt::Application.config.session_store :cookie_store, key: '_exsemt_session'
